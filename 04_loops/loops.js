f6a18570dedf33af71ce619a10c9ffb76e3f7114
Nirmohi <nirmohi0605@gmail.com>
2015-05-18 19:49:05 -0500

    updated commitfor loops.js

--- 04_loops/loops.js
+++ 04_loops/loops.js
@@ -8,7 +8,7 @@ function repeat(word, count){
 
 function sum(array){
 	var sum = 0;
-	for(var i = 0; i<array.length; i++){
+	for(var i = 0; i < array.length; i++){
 		sum += array [i];
 	}
 	return sum;
