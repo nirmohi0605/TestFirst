9f9708bb26c982f1eda3acd31c616abcd6f3f47d
Nirmohi <nirmohi0605@gmail.com>
2015-05-26 22:04:17 -0500

    commit for functions.js

--- 05_functions/functions.js
+++ 05_functions/functions.js
@@ -1,1 +1,62 @@
 // Functions.js
+
+var concat_string = function (){
+	var myArr = Array.prototype.slice.call(arguments);
+	myArr = myArr.join("");
+
+	return myArr;
+}
+
+var yourFunctionRunner = function(){
+	var newArr = Array.prototype.slice.call(arguments);
+		var str = [];
+	for (var i = 0; i < newArr.length; i++){
+		str.push(newArr[i]());
+
+	}
+
+	str = str.join("");	
+
+	return str;
+}
+
+
+var makeAdder = function (A){
+
+
+	var myFunc = function(n){
+
+		return (n + A);
+	}
+
+	return myFunc;
+}
+
+function once(func){
+    var param = 1;
+    return function(){
+        if(param === 1){
+         
+           func();   
+        }
+        param++;
+    };
+}
+
+function createObjectWithTwoClosures(){
+    var res = 0;
+    var object = {};
+       object.oneIncrementer = function(){
+           res += 1;
+       };
+       object.tensIncrementer = function(){
+           res += 10;
+       };
+       object.getValue=  function(){
+           return res;
+       };
+   
+   return object;
+
+
+}
